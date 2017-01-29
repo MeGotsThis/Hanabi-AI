118fca9d0186349ca566d86a400b21ac3f2cd3d3
MeGotsThis <megotsthis@gmail.com>
2017-01-28 23:55:18 -0800

    Hide the prints in random bot

--- randombot/bot.py
+++ randombot/bot.py
@@ -16,6 +16,10 @@ class Bot(bot.Bot):
     '''
     BOT_NAME = 'Random Bot'
 
+    def __init__(self, game, position, name, *, print_actions=False, **kwargs):
+        super().__init__(game, position, name, **kwargs)
+        self.printActions = print_actions
+
     def decide_move(self, can_clue, can_discard):
         actions = [PLAY]
         if can_clue:
@@ -55,57 +59,71 @@ class Bot(bot.Bot):
                 break
 
     def next_turn(self, player):
-        print('Current Turn: {}'.format(self.game.players[player].name))
+        if self.printActions:
+            print('Current Turn: {}'.format(self.game.players[player].name))
 
     def striked(self, player):
-        print('Striked: {}'.format(self.game.players[player].name))
-        print('Strike Count: {}'.format(self.game.strikeCount))
+        if self.printActions:
+            print('Striked: {}'.format(self.game.players[player].name))
+            print('Strike Count: {}'.format(self.game.strikeCount))
 
     def someone_drew(self, player, deckIdx):
-        print('Drew Card: {} {}'.format(self.game.players[player].name,
-                                        self.game.deck[deckIdx]))
+        if self.printActions:
+            print('Drew Card: {} {}'.format(self.game.players[player].name,
+                                            self.game.deck[deckIdx]))
 
     def someone_played(self, player, deckIdx, position):
-        print('Played Card: {} {} from slot {}'.format(
-            self.game.players[player].name, self.game.deck[deckIdx], position))
+        if self.printActions:
+            print('Played Card: {} {} from slot {}'.format(
+                self.game.players[player].name, self.game.deck[deckIdx],
+                position))
 
     def someone_discard(self, player, deckIdx, position):
-        print('Discarded Card: {} {} from slot {}'.format(
-            self.game.players[player].name, self.game.deck[deckIdx], position))
+        if self.printActions:
+            print('Discarded Card: {} {} from slot {}'.format(
+                self.game.players[player].name, self.game.deck[deckIdx],
+                position))
 
     def someone_got_color(self, from_, to, color, positions):
-        print('Got Color Clue: From {} To {}'.format(
-            self.game.players[from_].name, self.game.players[to].name))
-        print('{} On positions {}'.format(
-            color.full_name(self.game.variant),
-            ', '.join(str(p) for p in positions)))
+        if self.printActions:
+            print('Got Color Clue: From {} To {}'.format(
+                self.game.players[from_].name, self.game.players[to].name))
+            print('{} On positions {}'.format(
+                color.full_name(self.game.variant),
+                ', '.join(str(p) for p in positions)))
 
     def someone_got_value(self, from_, to, value, positions):
-        print('Got Number Clue: From {} To {}'.format(
-            self.game.players[from_].name, self.game.players[to].name))
-        print('{} On positions {}'.format(
-            value.value, ', '.join(str(p) for p in positions)))
+        if self.printActions:
+            print('Got Number Clue: From {} To {}'.format(
+                self.game.players[from_].name, self.game.players[to].name))
+            print('{} On positions {}'.format(
+                value.value, ', '.join(str(p) for p in positions)))
 
     def got_color_clue(self, player, color, positions):
-        print('Got Color Clue: From {}'.format(
-            self.game.players[player].name))
-        print('{} On positions {}'.format(
-            color.full_name(self.game.variant),
-            ', '.join(str(p) for p in positions)))
+        if self.printActions:
+            print('Got Color Clue: From {}'.format(
+                self.game.players[player].name))
+            print('{} On positions {}'.format(
+                color.full_name(self.game.variant),
+                ', '.join(str(p) for p in positions)))
 
     def got_value_clue(self, player, value, positions):
-        print('Got Number Clue: From {}'.format(
-            self.game.players[player].name))
-        print('{} On positions {}'.format(
-            value.value, ', '.join(str(p) for p in positions)))
+        if self.printActions:
+            print('Got Number Clue: From {}'.format(
+                self.game.players[player].name))
+            print('{} On positions {}'.format(
+                value.value, ', '.join(str(p) for p in positions)))
 
     def card_played(self, deckIdx, position):
-        print('Played Card: {} from slot {}'.format(
-            self.game.deck[deckIdx], position))
+        if self.printActions:
+            print('Played Card: {} from slot {}'.format(
+                self.game.deck[deckIdx], position))
 
     def card_discarded(self, deckIdx, position):
-        print('Discarded Card: {} from slot {}'.format(
-            self.game.deck[deckIdx], position))
+        if self.printActions:
+            print('Discarded Card: {} from slot {}'.format(
+                self.game.deck[deckIdx], position))
 
     def card_revealed(self, deckIdx):
-        print('Revealed Card: {}'.format(self.game.deck[deckIdx]))
+        if self.printActions:
+            print('Revealed Card: {}'.format(self.game.deck[deckIdx]))
