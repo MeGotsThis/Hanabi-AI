5a30da09f54790a2317ce52811530f799fe46981
MeGotsThis <megotsthis@gmail.com>
2017-02-08 17:13:54 -0800

    Added statistics of median, standard deviation, and standard error
    Also corrected simulator again

--- simulator.py
+++ simulator.py
@@ -4,7 +4,9 @@ import time
 
 from collections import ChainMap
 from datetime import datetime
+from math import sqrt
 from multiprocessing import Pool
+from statistics import mean, median, stdev
 
 from server import ServerGame
 from enums import Rank, Variant
@@ -16,6 +18,10 @@ def run(args):
     game.run_game()
     return game
 
+def iterateScores(scores):
+    for score, count in scores.items():
+        for _ in range(count):
+            yield score
 
 def simulate(config, mapFunc):
     bot = importlib.import_module(config['BOT']['bot'] + '.bot').Bot
@@ -51,9 +57,9 @@ def simulate(config, mapFunc):
     doneScores = {s: 0 for s in range(maxScore + 1)}
     lossScores = {s: 0 for s in range(maxScore + 1)}
 
-    check = min(100, runs // 100)
+    check = max(min(100, runs // 100), 1)
     completed = 0
-    for game in mapFunc:
+    for game in mapFunc(run, allArgs):
         completed += 1
         scores = doneScores if not game.loss else lossScores
         scores[game.score] += 1
@@ -64,6 +70,11 @@ def simulate(config, mapFunc):
         print('{}/{} completed, Current Time: {}'.format(
             completed, runs, datetime.now()))
     duration = time.time() - start
+    allScores = {s: doneScores[s] + lossScores[s] for s in range(maxScore + 1)}
+
+    scoreDisplay = (('All Games', allScores),
+                    ('Non-Loss Games', doneScores),
+                    ('Loss Games', lossScores))
 
     print()
     print('Bot: {}'.format(bot.BOT_NAME))
@@ -73,17 +84,24 @@ def simulate(config, mapFunc):
     print('Number of Games: {}'.format(completed))
     print('Number of Best Score: {}'.format(doneScores[maxScore]))
     print('Number of Losses: {}'.format(sum(lossScores.values())))
-    average = (sum(s * c for s, c in doneScores.items())
-               + sum(s * c for s, c in lossScores.items())) / completed
-    print('Average Score (all): {:.5f}'.format(average))
-    count = sum(doneScores.values())
-    average = (sum(s * c for s, c in doneScores.items())
-               / sum(doneScores.values())) if count != 0 else 0
-    print('Average Score (non-loss): {:.5f}'.format(average))
-    count = sum(lossScores.values())
-    average = (sum(s * c for s, c in lossScores.items())
-               / sum(lossScores.values())) if count != 0 else 0
-    print('Average Score (loss): {:.5f}'.format(average))
+    for scoreType, scores in scoreDisplay:
+        print()
+        print(scoreType)
+        average = 0
+        middle = 0
+        stdDev = 0
+        error = 0
+        count = sum(scores.values())
+        if count >= 1:
+            average = mean(iterateScores(scores))
+            middle = median(iterateScores(scores))
+        if count >= 2:
+            stdDev = stdev(iterateScores(scores))
+            error = stdDev / sqrt(count)
+        print('Average Score: {:.5f}'.format(average))
+        print('Median Score: {:.5f}'.format(middle))
+        print('Standard Deviation: {:.5f}'.format(stdDev))
+        print('Standard Error: {:.5f}'.format(error))
     print()
     print('Non-loss Score Distribution')
     for score in range(maxScore, -1, -1):
